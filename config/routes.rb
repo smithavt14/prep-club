b422967e8c9f457d34c7c06d60f8300ce95224a8
Alex Smith <smithavt14@gmail.com>
2018-08-07 12:39:48 +0800

    Added the show page, controller, and pundit policy for User model.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :users, only: [:show]
   resources :preps do
     resources :bookings, only: [ :index, :show, :new, :create, :destroy]
     resources :reviews, only: [ :index, :show, :new, :create, :destroy]
