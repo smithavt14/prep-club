f668b65e5bebba4280959e3cd4a63a838ffae796
Alex Smith <smithavt14@gmail.com>
2018-08-10 09:50:50 +0800

    Added photos to user and prep in the seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
+
+  def existing_prep?
+    @user.prep.nil?
+  end
 end
