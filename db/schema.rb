733381fb621412bdda2a0f143b64a4ea5b16efb4
Calvin Cheung <calvchum@gmail.com>
2018-08-14 15:29:16 +0800

    removed opacity from info banner

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(version: 2018_08_14_062559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
-    t.float "latitude"
-    t.float "longitude"
     t.string "recipe_name"
     t.string "recipe_url"
     t.text "recipe_ingredients"
