4db79bed992d2e0462b38271b20dd272b88563bc
Alex Smith <smithavt14@gmail.com>
2018-08-17 14:27:13 +0800

    Changed map to generate on the Julu Road

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,8 +24,8 @@ popupsearch();
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 31.3204, lng: 121.4737 });
-  map.setZoom(10);
+  const map = new GMaps({ el: '#map', lat: 31.2316207885742, lng: 121.48461151123 });
+  map.setZoom(12);
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   if (markers.length === 0) {
