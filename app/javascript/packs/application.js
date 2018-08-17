2ecd2159a4e793e0e3ee2a89dff3e16839d5d80d
Alex Smith <smithavt14@gmail.com>
2018-08-17 14:24:54 +0800

    Changed the map to generate on Shanghai

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,14 +24,15 @@ popupsearch();
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const map = new GMaps({ el: '#map', lat: 31.3204, lng: 121.4737 });
+  map.setZoom(10);
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
     map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
+    map.setZoom(13);
   } else {
     map.fitLatLngBounds(markers);
   }
